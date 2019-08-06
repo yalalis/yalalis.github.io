5d5fd3a999f715eabb278f2a1c96788fdf0dbf8f
Lisa Yala <yalalis@amazon.com>
2019-08-06 15:58:27 -0700

    cleaning up code

--- main.js
+++ main.js
@@ -5,18 +5,17 @@ target.appendChild(newScript);
 
 var dashboard
 var url
+var apiUrl = 'https://xzjsawhllk.execute-api.us-east-1.amazonaws.com/test/dashboard';
 function loadDashboard() {
 
-    callApi('https://xzjsawhllk.execute-api.us-east-1.amazonaws.com/test/dashboard', formatRequestBody(getParameters()))
+    callApi(apiUrl, formatRequestBody(getParameters()))
       .then(data => {
         console.log(JSON.stringify(data));
         url = getUrl(JSON.stringify(data));
-        //document.getElementById("apiResponse").innerHTML = url;
         embedDashboard(url);
         //getQR(url);
       })
       .catch(error => console.error(error));
-
 }
 
 
@@ -47,31 +46,20 @@ function embedDashboard(urlQuicksight) {
         width: "1000px"
     };
     dashboard = QuickSightEmbedding.embedDashboard(options);
-    dashboard.on("error", onError);
-    dashboard.on("load", onDashboardLoad);
-}
-
-function onDashboardLoad(payload) {
-    console.log("Do something when the dashboard is fully loaded.");
-}
-
-function onError(payload) {
-    console.log("Do something when the dashboard fails loading");
 }
 
 function getParameters() {
-// www.mysite.com/my_app.html?visualizationId=Machine1015?dashboardGroupId=MaintenanceDashboards?proximity=5
+    // www.mysite.com/index.html?visualizationId=Machine1015?dashboardGroupId=MaintenanceDashboards?proximity=5
     var GET = {};
     var query = window.location.search.substring(1).split("?");
     for (var i = 0, max = query.length; i < max; i++)
     {
-        if (query[i] === "") // check for trailing & with no param
+        if (query[i] === "")
             continue;
 
         var param = query[i].split("=");
         GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
     }
-    console.log(GET);
     return GET;
 }
 
@@ -85,49 +73,10 @@ function formatRequestBody(parameters = {}) {
            }
 }
 
- function getQR (url) {
+function getQR (url) {
     var newScript = document.createElement("script");
     var inlineScript = document.createTextNode('new QRCode("qrcode", "\''+url+'\'");');
     newScript.appendChild(inlineScript);
     var target = document.getElementById('qrcode');
     target.appendChild(newScript);
-    }
-
-
-//curl -i -X OPTIONS -H "Origin: https://sapp.amazon.com/DashboardEmbedding/" \
-//    -H 'Access-Control-Request-Method: POST' \
-//    -H 'Access-Control-Request-Headers: Content-Type : application/json, X-Api-Key: JslUpKkTuZ6w95ceN0XNw50PjIm5RZh57XnG9cLB' \
-//    "https://btvqh7qcec.execute-api.us-east-1.amazonaws.com/TEST/dashboard"
-
-
-//    try {
-//        var apiKey = "JslUpKkTuZ6w95ceN0XNw50PjIm5RZh57XnG9cLB";
-//        var request = {visualizationId: "Machine1015", dashboardGroupId: "MaintenanceDashboards", requestDistance: {proximity: 5.0}};
-//        const response1 = fetch('https://3i78jyqovb.execute-api.us-east-1.amazonaws.com/test/dashboard', {
-//                                    method: 'POST',
-//                                    mode: 'cors',
-//                                    headers: new Headers({
-//                                                     'Content-Type': 'application/json',
-//                                                     'X-Api-Key': 'JslUpKkTuZ6w95ceN0XNw50PjIm5RZh57XnG9cLB'
-//                                                 }),
-//                                    body: JSON.stringify(request)
-//                                });
-//         var delayInMilliseconds = 30000; //30 second
-//
-//         setTimeout(function() {
-//           console.log("Is this doing anything? fetch: ", response1);
-//         }, delayInMilliseconds);
-//        //console.log("Is this doing anything? fetch: ", response1);
-//    } catch (error) {
-//        console.error("This is definitely an error errorFetch: ", error);
-//    }
-
-
-
-//{
-//                                                                                          "visualizationId": "Machine1015",
-//                                                                                          "dashboardGroupId": "MaintenanceDashboards",
-//                                                                                          "requestDistance": {
-//                                                                                              "proximity": 5.0
-//                                                                                          }
-//                                                                                      }
+}
